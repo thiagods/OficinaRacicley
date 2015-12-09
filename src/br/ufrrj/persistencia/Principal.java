ce479f204f43d111be1db41aadb0ce8757649330
thiagods <thiago_d.s@hotmail.com>
2015-12-09 17:10:09 -0200

    Tentando adicionar tabela reparo

--- src/br/ufrrj/persistencia/Principal.java
+++ src/br/ufrrj/persistencia/Principal.java
@@ -13,6 +13,8 @@ public class Principal {
 		System.out.println("dropou carro:"+c.executar("drop table carro"));
 		System.out.println("dropou cliente:"+c.executar("drop table cliente"));
 		System.out.println("dropou fornecedor:"+c.executar("drop table fornecedor"));
+		System.out.println("dropou reparo:"+c.executar("drop table reparo"));
+
 		
 		//eu criei essas 4 tabelas.
 		//isso funciona
@@ -21,6 +23,7 @@ public class Principal {
 		System.out.println("cr carro: "+c.executar("create table carro(placa VARCHAR(8) PRIMARY KEY NOT NULL, marca VARCHAR(255), cor VARCHAR(255), ano INT, modelo VARCHAR(255), cpf_cliente VARCHAR(12) NOT NULL, FOREIGN KEY (cpf_cliente) references cliente(cpf));"));
 		System.out.println("cr fabri:"+c.executar("create table fabricante(id INT auto_increment PRIMARY KEY NOT NULL, nome VARCHAR(255), telefone VARCHAR(255))"));
 		System.out.println("cr forne:"+c.executar("create table fornecedor(id INT auto_increment NOT NULL, telefone VARCHAR(255), nome_vendedor VARCHAR(255), id_endereco INT NOT NULL, FOREIGN KEY (id_endereco) references endereco(id))"));
+		System.out.println("cr reparo:"+c.executar("create table reparo(id INT auto_increment NOT NULL, descricao_breve VARCHAR(255), descricao_detalhada VARCHAR(255), tempoMedioDeExecucao VARCHAR(255),valorMaoDeObra DOUBLE NOT NULL;"));
 	}
 	
 	public static void main(String[] args) throws SQLException {
